0a42a1c52c8dc86e194ea65f0678c30e58ea128b
Andy Gordon <andy@kujilabs.com>
2018-03-13 19:08:01 +0000

    new attempt

--- _artwork/jednotka/readme.txt
+++
@@ -1,4 +0,0 @@
-Hello, I’m Jednotka(v1.8)
-
-Documentation is located in html/docs.html.
-Images are not included in the main download.
--- _artwork/jednotka/ruby-on-rails/application.js
+++
@@ -1,5 +0,0 @@
-//= require jednotka/jquery/jquery.min
-//= require jednotka/jquery/jquery.mobile.custom.min
-//= require jednotka/bootstrap/bootstrap.min
-//= require jednotka/plugins/_plugins
-//= require jednotka/jednotka
